38749a0629ec348542406aa322698a28b56cdf92
afahrer <Stamkos91>
2019-07-08 12:19:56 -0400

    Assignment 2: Done

--- src/main/java/Assignment2/Assignment2.java
+++ src/main/java/Assignment2/Assignment2.java
@@ -1,5 +1,34 @@
 package Assignment2;
+/*
 
+Author:     Adam Fahrer
+Date:       July 8, 2019
+Input:      User provides input for the various algorithm that
+            can be ran using the menu interface
+
+How to use: Enter the number of the program you want to run
+            then enter input data for the selected algorithm
+
+Exceptions: Stack Overflow Error if the number of attendees shaking hands is very large
+
+Methods:    changeAt(String str) :
+                replaces "at" with "@" in a given string. Not case sensitive.
+            countHandshakes(int attendees) :
+                count the number of handshakes for a given number of attendees.
+            countTheStars(int numInLastRow):
+                count the total stars in a pyramid given the number of stars in the bottom row.
+            printStarsInPyramid(int numInLastRow):
+                public method that calls printStarsInPyramid(int stars, int spaces) to print the pyramid
+            printStarsInPyramid(int stars, int spaces):
+                private method called by printStarsInPyramid(int numInLastRow).
+                Needed to keep track of stars and spaces while printing the pyramid.
+            payThisWeek(int weeks):
+                calculates salary for the given week. Salary starts at $0.01 and doubles every week.
+                Maximum number of weeks is 52.
+            totalPayToDate(int weeks):
+                calls payThisWeek(int weeks) for every week including the week provided.
+                Returns total sum of earnings to date.
+*/
 
 import java.io.File;
 import java.math.BigDecimal;
@@ -8,10 +37,12 @@ import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Menu {
+public class Assignment2 {
     private static Scanner scanner = new Scanner(System.in);
-    // https://stackoverflow.com/questions/5323502/how-to-set-thousands-separator-in-java/12682507
+    //https://stackoverflow.com/questions/5323502/how-to-set-thousands-separator-in-java/12682507
     private static NumberFormat moneyFormatter = NumberFormat.getInstance(new Locale("en_US"));
+
+    //Looping Menu For running the programs within
     public static void main(String[] args) {
         int option;
         do {
@@ -29,9 +60,9 @@ public class Menu {
                     System.out.println("Original String: " + testAt + "\nAt replaced: " + changeAt(testAt) + "\n");
                     break;
                 case 2:
-                    System.out.println("Number of Athletes: ");
-                    int athletes = tryInt();
-                    System.out.println("Number of Athleles: " + athletes + "\nNumber of Handshakes: " + countHandshakes(athletes) + "\n");
+                    System.out.println("Number of attendees: ");
+                    int attendees = tryInt();
+                    System.out.println("Number of Athleles: " + attendees + "\nNumber of Handshakes: " + countHandshakes(attendees) + "\n");
                     break;
                 case 3:
                     System.out.println("Number of stars in last row: ");
@@ -58,9 +89,9 @@ public class Menu {
                 default:
                     option = 0;
             }
-        }while (option != 0);
+        } while (option != 0);
     }
-
+    //Try Catch for getting integers from a user
     private static int tryInt() {
         int num;
         try {
@@ -74,37 +105,39 @@ public class Menu {
         }
         return num;
     }
-
-    private static String changeAt(String str) {
+    //Part A
+    public static String changeAt(String str) {
         if (str.length() <= 1) return str;
         if (str.substring(0, 2).toLowerCase().equals("at")) return "@" + changeAt(str.substring(2));
         return str.substring(0, 1) + changeAt(str.substring(1));
     }
-
-    private static int countHandshakes(int numOfAttendees) {
+    //Part B
+    public static int countHandshakes(int numOfAttendees) {
         if (numOfAttendees <= 1) return 0;
         return numOfAttendees - 1 + countHandshakes(numOfAttendees - 1);
     }
-
-    private static int countTheStars(int numInLastRow) {
+    //Part C
+    public static int countTheStars(int numInLastRow) {
         if (numInLastRow <= 1) return 1;
         return numInLastRow + countTheStars(numInLastRow - 1);
     }
+    //Part D
+    public static void printStarsInPyramid(int numInLastRow) {
+        printStarsInPyramid(1, numInLastRow);
+    }
 
-    private static int starsInRow;
-    private static void printStarsInPyramid(int numInLastRow) {
-        if (numInLastRow <= 0) {
-            System.out.println("\nNumber of Stars: " + countTheStars(starsInRow) + "\n");
-            starsInRow = 0;
+    private static void printStarsInPyramid(int stars, int spaces) {
+        if (spaces <= 0) {
+            System.out.println("\nThe total number of stars is: " + countTheStars(stars) + "\n");
             return;
         }
         // https://stackoverflow.com/questions/2255500/can-i-multiply-strings-in-java-to-repeat-sequences
-        System.out.println(new String(new char[numInLastRow]).replace("\0", " ") +
-                new String(new char[++starsInRow]).replace("\0", "* "));
-        printStarsInPyramid(numInLastRow - 1);
+        System.out.println(new String(new char[spaces]).replace("\0", " ") +
+                new String(new char[stars]).replace("\0", "* "));
+        printStarsInPyramid(stars + 1, spaces - 1);
     }
-
-    private static int countFiles(String directory) {
+    //Part E
+    public static int countFiles(String directory) {
         File fileDirectory = new File(directory);
         if(fileDirectory.listFiles() == null) return 0;
         int files = 0;
@@ -114,13 +147,13 @@ public class Menu {
         }
         return files;
     }
-
-    private static long payThisWeek(int i) {
+    //Part F
+    public static long payThisWeek(int i) {
         if(i <= 1) return 1;
         return payThisWeek(i - 1)*2;
     }
 
-    private static long totalPayToDate(int i) {
+    public static long totalPayToDate(int i) {
         if(i <= 1) return 1;
         return payThisWeek(i) + totalPayToDate(i-1);
     }
